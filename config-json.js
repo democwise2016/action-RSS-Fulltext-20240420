e6763f764e5fc81b5564c2365656ce0384c8a010
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-04-20 23:35:26 +0800

    西元2024年04月20日 (週六) 23時35分26秒 CST

--- config-json.js
+++ config-json.js
@@ -21,7 +21,7 @@ let CONFIG = {
 
   maxFileCache: 2000,
 
-  proxy: 'https://script.google.com/macros/s/AKfycbxvQlZuQNHnxAcEmzJI9GxuiEAWYs3uPq2BmCvpkbpABKNKoc-Ahdqo6mqh8zzl-4nc/exec?proxy=',
+  proxy: 'https://script.google.com/macros/s/AKfycbzvvbn-FepcAJl1Ro3tgo0P4dXotOl81d4hcp4n4fNu_2RASYtO1idg6uxi0m7SXWtOMw/exec?proxy=',
   // feedList
   debug: false
 }
