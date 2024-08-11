cb1ffb22238ccde1442c17ec5d1a9ed1f29b6ca7
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-11 19:13:22 +0800

    西元2024年08月11日 (週日) 19時13分22秒 CST

--- config-list.js
+++ config-list.js
@@ -308,20 +308,20 @@ let feedList = [
 
 // ----------------------------------------------------------------
 
-feedList = [
-  { // <outline type="rss" text="Linuxeden开源社区++$0413-0909$" title="Linuxeden开源社区++$0413-0909$" xmlUrl="http://pulipuli.myqnapcloud.com/304/fc/$0413-0909$,/http%3A%2F%2Fwww.linuxeden.com%2Ffeed" htmlUrl="http://www.linuxeden.com"/>
-    title: 'Chrome Unboxed',
-    feedID: 'chromeunboxed',
-    feedURL: 'https://chromeunboxed.com/feed/',
-    itemFilters: [
-      ItemFilters['sites'].ItemFilterBlog,
-      ItemFilters['common'].ItemFilterImageEmbed,
-      ItemFilters['common'].ItemFilterThumbnailCache,
-    ],
-    options: {
-      selector: 'article > .entry-content'
-    }
-  },
-]
+// feedList = [
+//   { // <outline type="rss" text="Linuxeden开源社区++$0413-0909$" title="Linuxeden开源社区++$0413-0909$" xmlUrl="http://pulipuli.myqnapcloud.com/304/fc/$0413-0909$,/http%3A%2F%2Fwww.linuxeden.com%2Ffeed" htmlUrl="http://www.linuxeden.com"/>
+//     title: 'Chrome Unboxed',
+//     feedID: 'chromeunboxed',
+//     feedURL: 'https://chromeunboxed.com/feed/',
+//     itemFilters: [
+//       ItemFilters['sites'].ItemFilterBlog,
+//       ItemFilters['common'].ItemFilterImageEmbed,
+//       ItemFilters['common'].ItemFilterThumbnailCache,
+//     ],
+//     options: {
+//       selector: 'article > .entry-content'
+//     }
+//   },
+// ]
 
 module.exports = feedList
